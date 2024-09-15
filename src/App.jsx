df405359e426582bd2ba238c073ec19daa7d6043
khoji-7 <nematxonovh@gmail.com>
2024-09-15 22:23:30 +0500

    add hero

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Header from "./components/heading/header";
 import {BrowserRouter as Router, Switch} from "react-router-dom";
 import "./App.css"
+import { Route } from "react-router-dom/cjs/react-router-dom.min";
+import Home from "./components/home/Home";
 const App = () => {
     return (
         <>
             <Router>
                 <Header />
                 <Switch>
-
+                    <Route path="/" exact component={Home} />
                 </Switch>
             </Router>
         </>
---
+++ src/components/home/hero/Hero.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import "./hero.css";
+import Title from '../../title/Title';
+
+const Hero = () => {
+  return (
+    <>
+        <section className='hero'>
+            <div className='container'>
+                <div className='row'>
+                    <Title subtitle="WELCOME TO ACADEMIA" title="Best Online Education"/>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum tempore deserunt obcaecati voluptas maxime hic praesentium perspiciatis nesciunt excepturi nam laboriosam aliquid dolor ratione facere eos optio, odit voluptatibus corporis!
+                    </p>
+                    <div className='button'>
+                        <button className='primary-btn'>
+                            GET STARTED NOW <i className='fa fa-long-arrow-alt-right'></i>
+                        </button>
+                        <button >
+                            VIEW COURSE <i className='fa fa-long-arrow-alt-right'></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div className='margin'>
+
+        </div>
+    </>
+  )
+}
+
+export default Hero
